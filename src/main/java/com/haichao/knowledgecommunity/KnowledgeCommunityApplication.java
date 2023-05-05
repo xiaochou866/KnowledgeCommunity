939e889f0b5cc1200471175ae7f0cb7d8b11ab2b
Joker <1574427203@qq.com>
2023-05-05 21:16:49 +0800

    Initial commit

---
+++ src/main/java/com/haichao/knowledgecommunity/KnowledgeCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.haichao.knowledgecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haichao/knowledgecommunity/KnowledgeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haichao.knowledgecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
